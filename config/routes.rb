892e32161d4e7ad2eead474d789faa74fb9804bb
PatriciaMuench <pmuench89@gmail.com>
2018-04-09 15:35:54 -0400

    Continued setup of multiple Devise user models (Currently have some relevant notes in Evernote)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  
-  resources :reservations
-  devise_for :customers
-  devise_for :users
-  root 'restaurants#index'
 
   resources :restaurants
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'restaurants#index'
 
   get 'restaurants/:id/preview', to: 'restaurants#preview'
 
+  resources :reservations
+
+  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' }
+  # naming routes is supposed to result in: eg. http://localhost:3000/users/sign_in
+  # I have some questions/confusion about whether I am correctly overriding routes for users controllers... [some written in EverNote]
+
+  devise_for :customers, path: 'customers', controllers: { sessions: 'customers/sessions' }
+  # eg. http://localhost:3000/cutomers/sign_in
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
