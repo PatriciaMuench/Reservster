5c2a944083c7eb0b1f58108e705f09e57ffbf5da
PatriciaMuench <pmuench89@gmail.com>
2018-04-29 15:44:43 -0400

    Cleaned up code and comments, mostly from trying to fix reservation creation errors

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,14 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
 
-  # trying this to help with creation errors:
+  # to help with creation errors:
   before_action :set_restaurant_for_create, only: [:create]
 
   before_action :authenticate_customer!
-  # Is it OK not to have only or except? - yes
 
   before_action :authorize_customer, only: [:show, :edit, :update, :destroy]
 
   # add rules regarding users (owners) CRUD'ing reservations?
-
-  # trying this to help with creation errors:
-  # before_action :set_restaurant, only: [:new]
 
 
   # GET /reservations
@@ -30,13 +26,8 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
 
-    # trying this to help with creation errors:
-    # @reservation.restaurant = @restaurant
-    # @reservation.restaurant = Restaurant.find(params[:restaurant][:id])
-    # @reservation.restaurant = Restaurant.find(params[:restaurant_id])
+    # to help with creation errors:
     @restaurant = Restaurant.find(params[:restaurant_id])
-
-
   end
 
   # GET /reservations/1/edit
@@ -48,14 +39,9 @@ class ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new(reservation_params)
 
-    # trying this to help with creation errors:
-    @reservation.customer = current_customer # this one seems to be working
-    # same as doing @restaurant.user_id = current_user.id
-    # @reservation.restaurant = current_restaurant
-    # @reservation.restaurant = restaurant
-    @reservation.restaurant = @restaurant # this one doesn't work yet - ?
-    # @reservation.restaurant_id = @restaurant.id
-    # @reservation.restaurant_id = @restaurant
+    # to help with creation errors:
+    @reservation.customer = current_customer 
+    @reservation.restaurant = @restaurant 
 
     respond_to do |format|
       if @reservation.save
@@ -99,24 +85,13 @@ class ReservationsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    # trying this to help with creation errors (comment out restaurant_id and customer_id):
     def reservation_params
       params.require(:reservation).permit(:date, :time, :party_size, :restaurant_id, :customer_id)
     end 
 
-    # trying this to help with creation errors:
-    # def set_restaurant
-    #   @restaurant = Restaurant.find(params[:restaurant_id])
-    # end
-
+    # to help with creation errors:
     def set_restaurant_for_create
       @restaurant = Restaurant.find(params[:restaurant][:id])
-      # @restaurant = Restaurant.find(params[:restaurant_id])
-      # @restaurant = params[:restaurant][:id]
-      # @restaurant = params[:restaurant][:id].to_i
-      # @restaurant = Restaurant(params[:restaurant][:id])
-      # @restaurant.id = params[:restaurant_id]
-      # @restaurant = params[:restaurant_id]
     end
     # for reference, from views/restaurants/show: 
     # <%= link_to 'New Reservation', new_reservation_path(restaurant_id: @restaurant.id)%>
